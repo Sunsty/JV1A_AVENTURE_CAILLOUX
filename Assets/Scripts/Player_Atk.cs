0723960fb34ebe06348bfb95a5a1eaca99eef51f
Sunsty <cailloux.baptiste@gmail.com>
2024-04-03 12:02:20 +0200

    Player ATK ( hitbox + push )

---
+++ Assets/Scripts/Player_Atk.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class Player_Atk : MonoBehaviour
+{
+    public bool isColliding = false ;
+    public GameObject collidingWith;
+    public float hitForce;
+    public bool isHiting;
+
+    private float hitingCounter;
+    public float hitingLenght;
+
+    public Vector2 direction;
+    public Vector2 target;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        direction = transform.position - collidingWith.transform.position;
+
+        if (Input.GetKeyDown(KeyCode.C) && isColliding && !isHiting)
+        {
+            isHiting = true ;
+        }
+
+        if (isHiting)
+        {
+            if (hitingCounter <= 0)
+            {
+                hitingCounter = hitingLenght;
+            }
+            isHiting = false;
+
+            target = collidingWith.transform.position + (collidingWith.transform.position - transform.position);
+        }
+
+        if (hitingCounter > 0)
+        {
+            hitingCounter -= Time.fixedDeltaTime;
+
+            collidingWith.transform.position = Vector2.MoveTowards(collidingWith.transform.position, target, hitForce * Time.fixedDeltaTime);
+
+            if (hitingCounter < 0)
+            {
+
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            isColliding = true;
+            collidingWith = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            isColliding = false;
+        }
+    }
+}
